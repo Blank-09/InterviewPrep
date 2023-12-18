d5ac190d72cd212ffb7cebb968cb6b4d419e9cc4
Priyanshu T <priyanshu9943@gmail.com>
2023-12-18 22:32:56 +0530

    Updated Dependencies and added theme toggle

---
+++ constants/index.ts
@@ -0,0 +1,14 @@
+export const title = 'InterviewPrep'
+
+// Description
+export const description =
+  'InterviewPrep is a platform for developers to practice their coding skills and prepare for technical interviews.'
+
+// Keywords
+// prettier-ignore
+export const keywords = [
+  'interview', 'prep', 'interviewprep', 'coding', 'practice',
+  'technical', 'interview', 'questions', 'answers', 'solutions',
+  'problems', 'challenges', 'algorithms', 'data', 'structures',
+  'datastructures', 'algorithms'
+]
